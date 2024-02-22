0379c1fa67e3c65ea21af16b7ca918e0591ab53c
juhwan <asdcz@naver.com>
2024-02-22 18:50:38 +0900

    springSession 설정

---
+++ src/main/java/com/byeon/task/security/SessionConfig.java
@@ -0,0 +1,20 @@
+package com.byeon.task.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfig {
+    @Bean
+    public EmbeddedDatabase embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("org/springframework/session/jdbc/schema-h2.sql")
+                .build();
+    }
+}
