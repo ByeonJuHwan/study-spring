9dd8765117e4b1b5836b3cd554a12d3f7ec99fb5
juhwan <asdcz@naver.com>
2024-02-27 19:00:00 +0900

    AccessLog 필터추가

--- src/main/java/com/byeon/task/config/AppConfig.java
+++ src/main/java/com/byeon/task/config/AppConfig.java
@@ -1,11 +1,19 @@
 package com.byeon.task.config;
 
+import com.byeon.task.config.filter.AccessLogFilter;
+import com.byeon.task.repository.AccessLogRepository;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
     @Bean
@@ -16,5 +24,15 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    private final AccessLogRepository accessLogRepository;
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new AccessLogFilter(accessLogRepository));
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.setUrlPatterns(Collections.singleton("/*"));
+        return filterFilterRegistrationBean;
     }
 }
