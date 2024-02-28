6e618142fa47c8df3d6c7d0d3c637be5f5df43e6
juhwan <asdcz@naver.com>
2024-02-29 00:18:38 +0900

    마이크로초 저장, 사용자 아이디 저장

--- src/main/java/com/byeon/task/domain/entity/AccessLog.java
+++ src/main/java/com/byeon/task/domain/entity/AccessLog.java
@@ -1,11 +1,9 @@
 package com.byeon.task.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,6 +16,7 @@ public class AccessLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String userId;
     private String ipAddress;
     private String userAgent;
     private LocalDateTime requestTime;
@@ -29,5 +28,8 @@ public class AccessLog {
     //  String responseBody; 추가필요
     private String responseBody;
     //  int elapseTime; 추가필요 : 얼마나걸렸느냐
-    private Integer elapseTime;
+    //private Integer elapseTime;
+
+    // 마이크로초까지 저장하기 위해서 변경
+    private Double elapseTime;
 }
