f9768ee8696dac24b57e365b55056ccffaeb883f
juhwan <asdcz@naver.com>
2024-02-29 12:25:28 +0900

    모든 uri 에 필터 적용

--- src/main/java/com/byeon/task/config/AppConfig.java
+++ src/main/java/com/byeon/task/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.byeon.task.config;
 import com.byeon.task.config.filter.AccessLogFilter;
 import com.byeon.task.config.filter.CheckLoginUserFilter;
 import com.byeon.task.repository.AccessLogRepository;
+import com.byeon.task.service.TelegramService;
 import com.byeon.task.service.threadlocal.ThreadLocalSaveUserID;
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,14 @@ public class AppConfig {
 
     private final AccessLogRepository accessLogRepository;
     private final ThreadLocalSaveUserID threadLocalSaveUserID;
+    private final TelegramService telegramService;
 
     @Bean
     public FilterRegistrationBean<Filter> accessLogFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new AccessLogFilter(accessLogRepository,threadLocalSaveUserID));
+        filterFilterRegistrationBean.setFilter(new AccessLogFilter(accessLogRepository,threadLocalSaveUserID,telegramService));
         filterFilterRegistrationBean.setOrder(2);
-        filterFilterRegistrationBean.setUrlPatterns(List.of("/translate/data"));    // todo 모든 API 에 대해서 엑세스로그를 남겨야할것 같습니다.
+        filterFilterRegistrationBean.setUrlPatterns(List.of("/*"));    // 모든 API 에 대해서 엑세스로그를 남겨야할것 같습니다.
         return filterFilterRegistrationBean;
     }
 
@@ -33,7 +35,7 @@ public class AppConfig {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new CheckLoginUserFilter(threadLocalSaveUserID));
         filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.setUrlPatterns(List.of("/translate/data"));    // todo 모든 API 에 대해서 엑세스로그를 남겨야 되면 수정바랍니다.
+        filterFilterRegistrationBean.setUrlPatterns(List.of("/*"));    // 모든 API 에 대해서 엑세스로그를 남겨야 되면 수정바랍니다.
         return filterFilterRegistrationBean;
     }
 }
