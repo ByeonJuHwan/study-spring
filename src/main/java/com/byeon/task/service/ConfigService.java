a35ff0c0cbfdf40cee8cb38352cdde563e0dc3f2
yoe21c <yoe21c@gmail.com>
2024-03-06 21:40:39 +0900

    * 코멘트 드립니다.

--- src/main/java/com/byeon/task/service/ConfigService.java
+++ src/main/java/com/byeon/task/service/ConfigService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 객체를 json 형태로 레디스에 넣어보는것도 해보면 좋을 것 같네요.
+ * 그럼 객체를 직렬화, 역직렬화해서 사용하는 일에도 쓰일수 있음을 알고 나면 다양한 곳에서도 사용할 수 있습니다.
+ * 그리고 Cacheable 통해서 캐시에 들어가게 되면 캐시에 어떤식으로 들어가는지를 확인해보면서 학습을 해주시면 더 좋을 것 같아요.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
